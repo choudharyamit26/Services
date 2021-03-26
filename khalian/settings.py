20c90bd6468f4184969bf0544397bc2739b6fd27
choudharyamit26 <ravichoudhary766@gmail.com>
2021-03-26 12:01:41 +0530

    Added postgresql database on aws

--- khalian/settings.py
+++ khalian/settings.py
@@ -81,10 +81,21 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 AUTH_USER_MODEL = 'adminpanel.User'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'khalian',
+        'USER': 'khalian_user',
+        'PASSWORD': 'khalian_user',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
