d9ca45803ea0ddae109f99ee0cfd47af3ce5b79b
choudharyamit26 <ravichoudhary766@gmail.com>
2021-08-04 18:14:08 +0530

    added hide category in adminpanel

--- khalian/settings.py
+++ khalian/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$$gx-y2sfsw7ft=yo%1(0ylch&fv@die+j02c!_6birzid8j1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
